8e0d3ccaf6da10110157db2bf2861cc96d83fad5
Christophe1 <harris.christophe@gmail.com>
2017-12-06 19:21:47 +0000

    1st commit

---
+++ main/java/com/example/chris/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.chris.collapsingtoolbar;
+
+/**
+ * Created by Chris on 04/12/2017.
+ */
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import static com.miguelcatalan.materialsearchview.R.styleable.MaterialSearchView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Material Search");
+        toolbar.setTitleTextColor(Color.parseColor("FFFFFF"));
+
+        MaterialSearchView searchView = (MaterialSearchView)findViewById(R.id.search_view);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
